7fac11985f3624a89ba7fd92c0aa5a64155278c8
Benjamin Carriere <carriere@info27-13.iut45.univ-orleans.fr>
2014-01-29 17:36:47 +0100

    Ajout du dossier models pour gérer des modèles

--- target/scala-2.10/src_managed/main/views/html/index.template.scala
+++ target/scala-2.10/src_managed/main/views/html/index.template.scala
@@ -30,7 +30,7 @@ Seq[Any](format.raw/*1.19*/("""
 
 """),_display_(Seq[Any](/*3.2*/main("Welcome to Play")/*3.25*/ {_display_(Seq[Any](format.raw/*3.27*/("""
 
-    """),_display_(Seq[Any](/*5.6*/play20/*5.12*/.welcome(message, style = "Java"))),format.raw/*5.45*/("""
+    
 
 """)))})),format.raw/*7.2*/("""
 """))}
@@ -45,11 +45,11 @@ Seq[Any](format.raw/*1.19*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Thu Jan 23 08:48:48 CET 2014
+                    DATE: Wed Jan 29 17:35:29 CET 2014
                     SOURCE: /home/carriere/Documents/lp/gestionProjet/burgerGame/app/views/index.scala.html
-                    HASH: c1c7a8094ff6fdf7b189be1a11ff9a14918bbdfd
-                    MATRIX: 774->1|885->18|922->21|953->44|992->46|1033->53|1047->59|1101->92|1134->95
-                    LINES: 26->1|29->1|31->3|31->3|31->3|33->5|33->5|33->5|35->7
+                    HASH: b475fb75d27c2095d9bc40a31a0fe69aea776c48
+                    MATRIX: 774->1|885->18|922->21|953->44|992->46|1031->55
+                    LINES: 26->1|29->1|31->3|31->3|31->3|35->7
                     -- GENERATED --
                 */
             
