3ac5c157c3e4b66b664f8821bc51d2727273bfd9
Benjamin Carriere <carriere@info27-30.iut45.univ-orleans.fr>
2014-02-13 14:25:11 +0100

    ajout de l'interface

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,11 +4,62 @@ import play.*;
 import play.mvc.*;
 
 import views.html.*;
+import models.Burgers;
+import java.util.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("titi"));
-    }
+	
+
+
+
+
+
+	public static Result index() {
+		return ok(index.render("")); // redirect page
+
+
+
+
+
+	}
+
+	public static Result submit() {
+		String ingredients_burger[] = {"Fromage", "Tomate", "Salade", "Oignon", "steak", "Cornichon", "Sauce Blanche"};
+    	Burgers bigmac = new Burgers("Bigmac", ingredients_burger);
+		String toto;
+       
+		//Map<String, String[]> map = request().body().asFormUrlEncoded();
+
+    	//String[] checkedVal = map.get("burger");
+
+		String tab_ingredients[] = {"Fromage", "Tomate", "Salade", "Oignon", "steak", "Cornichon", "Sauce Blanche"};
+    	if(tab_ingredients.length!=0)
+    	{
+	    	if(bigmac.compareIngredients(tab_ingredients))
+	    	{
+	    		toto="Félicitations, vous avez reconstitué le burger !";
+	    	}
+	    	else
+	    	{
+	    		toto="Retentez votre chance !";
+	    	}
+	    }
+	    else
+	    {
+	    	toto="Vous n'avez rien coché!!!!!!";
+	    }
+
+
+
+	    return ok(index.render(toto)); // redirect page
+
+
+
+
+
+	}
+
+
 
 }
